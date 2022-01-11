7f836b7ff4e57cb9cebe65914616c95b87175481
utkarsh bhaskar <utkarshbhaskar05@gmail.com>
2022-01-11 13:17:32 +0530

    server setup done

--- source/config/logging.ts
+++ source/config/logging.ts
@@ -1,56 +1,42 @@
-const getTimeStamp=():string=>{
-    return new Date().toISOString();
+const info = (namespace: string, message: string, object?: any) => {
+   if (object) {
+       console.log(`[${getTimeStamp()}] [INFO] [${namespace}] ${message}`, object);
+   } else {
+       console.log(`[${getTimeStamp()}] [INFO] [${namespace}] ${message}`);
+   }
 };
 
-
-const info=(namespace :string, message:string, object?:any)=>{
-   if(object)
-   {
-      console.log(`[${getTimeStamp}] [INFO] [${namespace}] ${message}`,object);
+const warn = (namespace: string, message: string, object?: any) => {
+   if (object) {
+       console.log(`[${getTimeStamp()}] [WARN] [${namespace}] ${message}`, object);
+   } else {
+       console.log(`[${getTimeStamp()}] [WARN] [${namespace}] ${message}`);
    }
-   else{
-      console.log(`[${getTimeStamp}] [INFO] [${namespace}] ${message}`);
+};
+
+const error = (namespace: string, message: string, object?: any) => {
+   if (object) {
+       console.log(`[${getTimeStamp()}] [ERROR] [${namespace}] ${message}`, object);
+   } else {
+       console.log(`[${getTimeStamp()}] [ERROR] [${namespace}] ${message}`);
    }
-}
+};
 
-const warn=(namespace :string, message:string, object?:any)=>{
-    if(object)
-    {
-       console.log(`[${getTimeStamp}] [WARN] [${namespace}] ${message}`,object);
-    }
-    else{
-       console.log(`[${getTimeStamp}] [WARN] [${namespace}] ${message}`);
-    }
- }
+const debug = (namespace: string, message: string, object?: any) => {
+   if (object) {
+       console.log(`[${getTimeStamp()}] [DEBUG] [${namespace}] ${message}`, object);
+   } else {
+       console.log(`[${getTimeStamp()}] [DEBUG] [${namespace}] ${message}`);
+   }
+};
 
+const getTimeStamp = (): string => {
+   return new Date().toISOString();
+};
 
- const error=(namespace :string, message:string, object?:any)=>{
-    if(object)
-    {
-       console.log(`[${getTimeStamp}] [ERROR] [${namespace}] ${message}`,object);
-    }
-    else{
-       console.log(`[${getTimeStamp}] [ERROR] [${namespace}] ${message}`);
-    }
- }
-
-
- const debug=(namespace :string, message:string, object?:any)=>{
-    if(object)
-    {
-       console.log(`[${getTimeStamp}] [DEBUG] [${namespace}] ${message}`,object);
-    }
-    else{
-       console.log(`[${getTimeStamp}] [DEBUG] [${namespace}] ${message}`);
-    }
- }
-
-
-
-
- export default{
-     info,
-     warn,
-     error,
-     debug
- };
+export default {
+   info,
+   warn,
+   error,
+   debug
+};
--- source/server.ts
+++ source/server.ts
@@ -7,4 +7,46 @@ import config from './config/config';
 const NAMESPACE='Server';
 const app=express();
 
-app.use()
+app.use((req,res,next)=>{
+
+    logging.info(NAMESPACE, `Method:  [${req.method}]  -URL: [${req.url}]  -IP: [${req.socket.remoteAddress}] `);
+
+    res.on('finish',()=>{
+  logging.info(NAMESPACE, `Method:  [${req.method}]  -URL: [${req.url}]  -IP: [${req.socket.remoteAddress}] -Status: [${res.statusCode}]`);
+    })
+
+    next();
+
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+app.use((req,res,next)=>{
+
+    res.header('A-C-A-Origin','*');
+    res.header('A-C-A-Header','Origin,X-requested-with,Content-Type,Accept,Authorization');
+
+    if(req.method=='OPTIONS')
+    {
+        res.header('A-C-A-Methods','PUT,POST,PATCH,DELETE,GET');
+        return res.status(200).json({});
+    }
+
+    next();
+});
+
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+
+    res.status(404).json({
+        message: error.message
+    });
+});
+
+
+const httpServer=http.createServer(app);
+
+httpServer.listen(config.server.port,()=>logging.info(NAMESPACE,`server is running on ${config.server.hostname}:${config.server.port}`) );
