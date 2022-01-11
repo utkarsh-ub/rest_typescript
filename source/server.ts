c0f1c350b76596d34197486fac9a6c3fdd3dbf07
utkarsh bhaskar <utkarshbhaskar05@gmail.com>
2022-01-11 12:08:24 +0530

    initial

---
+++ source/server.ts
@@ -0,0 +1,10 @@
+import http from 'http';
+import express from 'express';
+import bodyParser from 'body-parser';
+import logging from './config/logging';
+import config from './config/config';
+
+const NAMESPACE='Server';
+const app=express();
+
+app.use()
